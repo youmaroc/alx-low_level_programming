d300e26eb9dcf164f51020142227e5e7cba303f9
youmaroc <youssef.abdaoui@hotmail.com>
2023-06-20 12:43:49 -0700

    2-print_alphaa

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -9,10 +9,10 @@ void print_alphabet_x10(void)
 	int i;
 	int j;
 
-	for (j = 1;i <= 10; i++ )
+	for (j = 1;i <= 10; i++)
 	{	
 		
-		for (j = 97; j <= 122 ; j++)
+		for (j = 97; j <= 122; j++)
 		{
 			_putchar(j);
 		}
