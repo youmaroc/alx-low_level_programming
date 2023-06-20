240302fa4ae61676c516d05b5c2257b1cdec966a
youmaroc <youssef.abdaoui@hotmail.com>
2023-06-20 11:20:15 -0700

    0 task

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,13 +1,13 @@
 #include "main.h"
-	/**
-	 * main - print _putchar
-	 * is written by mbah
-	 * Return: 0
-	 */
+/**
+* main - print _putchar
+* is written by mbah
+* Return: always  0
+*/
 
 
-	int main(void)
-	{
+int main(void)
+{
 	_putchar('_');
 	_putchar('p');
 	_putchar('u');
@@ -18,4 +18,4 @@
 	_putchar('r');
 	_putchar('\n');
 	return (0);
-	}
+}
