ca5058bc8dac0ea25c70b4aa651010a5c5fd047c
youmaroc <youssef.abdaoui@hotmail.com>
2023-06-20 12:55:16 -0700

    2-print_alphaa

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,21 +1,19 @@
 #include "main.h"
+
 /**
 * print_alphabet_x10 - prints the alphabets 1O times.
 *
 * Return: always 0 (reussit)
 */
+
 void print_alphabet_x10(void)
 {
-	int i;
-	int j;
+	int line, ch;
 
-	for (j = 1;i <= 10; i++)
+	for (line = 0; line <= 9; line++)
 	{	
-		
-		for (j = 97; j <= 122; j++)
-		{
-			_putchar(j);
-		}
+		for (ch = 'a'; ch <= 'z'; ch++)
+			_putchar(ch);
 		_putchar('\n');
 	}
 }
