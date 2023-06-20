1f110c19a49d2ad8de79879450de00e271e02f09
youmaroc <youssef.abdaoui@hotmail.com>
2023-06-20 11:11:58 -0700

    0 AND 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+
+	/**
+	 * print_alpha - print all LETTERS in lowercase
+	 */
+
+
+	void print_alphabet(void)
+	{
+		char letter;
+
+
+		for (letter = 'a'; letter <= 'z'; letter++)
+			_putchar(letter);
+
+
+		_putchar('\n');
+	}
