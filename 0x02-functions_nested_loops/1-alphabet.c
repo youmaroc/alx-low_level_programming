e606e012aabc407c1670a93929a74ef3892bfe28
youmaroc <youssef.abdaoui@hotmail.com>
2023-06-20 11:30:42 -0700

    1 TASK

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,14 +1,16 @@
 #include "main.h"
 /**
 * print_alpha - print all LETTERS in lowercase
+*
+*Return : Always 0
 */
 void print_alphabet(void)
 {
-		char letter;
+		int i;
 
 
-		for (letter = 'a'; letter <= 'z'; letter++)
-			_putchar(letter);
+		for (i = 97; i <= 122; i++)
+			_putchar(i);
 
 
 		_putchar('\n');
