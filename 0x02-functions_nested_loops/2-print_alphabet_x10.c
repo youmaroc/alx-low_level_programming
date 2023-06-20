11cb4781f6130dc0f110f877d80a916906e326d6
youmaroc <youssef.abdaoui@hotmail.com>
2023-06-20 13:02:21 -0700

    2-print_alpha

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,9 +1,8 @@
 #include "main.h"
 
 /**
-* print_alphabet_x10 - prints the alphabets 1O times.
-*
-* Return: always 0 (reussit)
+ * print_alphabet_x10 - prints the alphabets 1O times.
+ *
 */
 
 void print_alphabet_x10(void)
