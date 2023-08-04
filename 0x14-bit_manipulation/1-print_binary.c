2a7109386fd15b8c44e3471e07e6b7037a4fdc96
youmaroc <youssef.abdaoui1985@gmail.com>
2023-08-04 15:47:29 -0700

    all task of project

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /**
- *how  print_binary - print the binary equiv  a decimal number
- * @n: number to print >> binary
+ * print_binary - prints the binary equivalent of a decimal number
+ * @n: number to print in binary
  */
 void print_binary(unsigned long int n)
 {
